f1c2437182f57d3b187771f504b44a4ec4f98377
Nitin T <nitintwbd@gmail.com>
2026-05-31 09:02:59 +0530

    feat: implement Fill program assembly logic

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,59 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+//PHASE 1: The Listener 
+(MAIN_LOOP)
+@KBD
+D=M
+@BLACK
+D;JGT
+@WHITE
+D;JEQ
+
+//PHASE 2: Color and Pointer Setup
+(BLACK)
+D=-1
+@color
+M=D
+@SCREEN
+D=A
+@pointer
+M=D
+@DRAW_LOOP
+0;JMP
+
+(WHITE)
+D=0
+@color
+M=D
+@SCREEN
+D=A
+@pointer
+M=D
+@DRAW_LOOP
+0;JMP
+
+//PHASE 3: The Drawing loop
+//1.Boundary Check
+(DRAW_LOOP)
+@KBD 
+D=A
+@pointer
+D=D-M
+@MAIN_LOOP
+D;JEQ
+
+//2.Derefercing and Paint
+@color
+D=M
+@pointer
+A=M
+M=D
+
+//3.Increment Pointer
+@pointer
+M=M+1
+
+//4.Repeat
+@DRAW_LOOP
+0;JMP
